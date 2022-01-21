4bf06881e0f77e3c89872c990a5aef33089dc4c5
Tenreiro Benjamin <47473393+tommysonsylverstone@users.noreply.github.com>
2022-01-21 16:31:38 +0100

    Delete src directory

--- src/Resources/js/components/SuluRange/index.js
+++
@@ -1,3 +0,0 @@
-import SuluRange from "./SuluRange";
-
-export default SuluRange;
--- src/Resources/js/index.js
+++
@@ -1,4 +0,0 @@
-import { fieldRegistry } from 'sulu-admin-bundle/containers'
-import SuluRange from './components/SuluRange/SuluRange';
-
-fieldRegistry.add('sulu_range', SuluRange);
